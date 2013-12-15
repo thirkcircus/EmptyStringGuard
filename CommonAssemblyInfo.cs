cdc487fea68579692465e50c853404dd3cbace8b
Justin Thirkell <justin.thirkell@xero.com>
2013-12-16 11:21:52 +1300

    install.ps1 ensures packages.config DevelopmentDependency attribute is set for this pkg

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("EmptyStringGuard")]
 [assembly: AssemblyProduct("EmptyStringGuard")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
